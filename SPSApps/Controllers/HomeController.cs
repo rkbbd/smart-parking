22e8468ac5c40639027b96f036e516a5307254c5
MD RAKIB HASAN <rakib424.com>
2023-11-21 23:54:35 +0600

    update

--- SPSApps/Controllers/HomeController.cs
+++ SPSApps/Controllers/HomeController.cs
@@ -35,6 +35,42 @@ namespace SPSApps.Controllers
             }
         }
 
+
+        public IActionResult Confirm(int emergency, int request)
+        {
+            var email = _session.GetString("email");
+            var name = _session.GetString("name");
+            var allLocation = _context.Buildings.FirstOrDefault(f=>f.Id == emergency || f.Id == request);
+            ConfirmDTO home = new ConfirmDTO(name, email, allLocation, emergency > 1);
+            if (email != null)
+            {
+                return View(home);
+            }
+            else
+            {
+                return View(new ConfirmDTO("name", "email", allLocation, false));//TODO Remove
+                return RedirectToAction("Login", "Users", new { login = true });
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Confirm(int Id)
+        {
+            var email = _session.GetString("email");
+            var name = _session.GetString("name");
+            var allLocation = _context.Buildings.FirstOrDefault(f => f.Id == Id);
+            ConfirmDTO home = new ConfirmDTO(name, email, allLocation, false);
+            if (email != null)
+            {
+                return View(home);
+            }
+            else
+            {
+                return View(new ConfirmDTO("name", "email", allLocation, false));//TODO Remove
+                return RedirectToAction("Login", "Users", new { login = true });
+            }
+        }
+
         [HttpPost]
         public async Task<IActionResult> Login(LoginDTO login)
         {
--- SPSApps/Models/Register/HomeDTO.cs
+++ SPSApps/Models/Register/HomeDTO.cs
@@ -3,4 +3,5 @@
 namespace SPSApps.Models.Register
 {
     public record HomeDTO(string Name, string Email, List<Building> Buildings);
+    public record ConfirmDTO(string Name, string Email, Building Buildings, bool isEmergency);
 }
